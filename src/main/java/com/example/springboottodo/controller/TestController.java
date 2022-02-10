178ac4bfd4bd13551b10fab1fcb2f937f8931d8a
limkk90 <44752388+limkk90@users.noreply.github.com>
2022-02-10 23:37:32 +0900

    first commit

---
+++ src/main/java/com/example/springboottodo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.springboottodo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @GetMapping
+    public String testController(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/testGetMapping")
+    public String testControllerWithPath(){
+        return "Hello World! testGetMapping";
+    }
+}
