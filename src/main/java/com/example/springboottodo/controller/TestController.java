0b26fe69ff6c5e7da500b94280068cbb516732f4
limkk90 <44752388+limkk90@users.noreply.github.com>
2022-02-12 23:42:11 +0900

    RequestParam

--- src/main/java/com/example/springboottodo/controller/TestController.java
+++ src/main/java/com/example/springboottodo/controller/TestController.java
@@ -1,9 +1,6 @@
 package com.example.springboottodo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("test")
@@ -22,5 +19,10 @@ public class TestController {
     public String testControllerWithPathVariables(@PathVariable(required = false) int id){
         //int id에 /test/123에서 123이 들어감
         return "Hello World! Id" + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testControllerRequestParam(@RequestParam(required = false) int id){
+        return "Hello WOrld! ID" + id;
     }
 }
