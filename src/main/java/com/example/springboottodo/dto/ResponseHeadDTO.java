3bb362232e4f783dc5a1af3b2221c7eb195d4d34
limkk90 <44752388+limkk90@users.noreply.github.com>
2022-02-20 23:18:55 +0900

    responseHeadDTO

--- src/main/java/com/example/springboottodo/dto/ResponseHeadDTO.java
+++ src/main/java/com/example/springboottodo/dto/ResponseHeadDTO.java
@@ -1,4 +1,11 @@
 package com.example.springboottodo.dto;
 
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ResponseHeadDTO {
+    private String error;
+    private List<T> data;
 }
